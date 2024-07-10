6ec04babefba2174cfec5daaf99e251f780f9edf
vimala2803 <vimalaelangovan28@gmail.com>
2024-07-10 16:20:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var on_button=document.getElementById("on");
+var off_button=document.getElementById("off");
+var image=document.getElementById("bulb");
+
+function bulb_on() {
+    image.src="./image/bulb_on.jpeg"
+}
+function bulb_off() {
+    image.src="./image/bulb_off.jpeg"
+}
